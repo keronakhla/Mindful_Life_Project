5a1ffeb85d1e1d8b61915c99b80d4e2c298cc21b
Chris Queen <chris@bitmatica.com>
2015-10-04 17:01:41 -0700

    Initial Commit

---
+++ MindfulLifeProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MindfulLifeProject
+//
+//  Created by Christopher Queen on 10/4/15.
+//  Copyright © 2015 Christopher Queen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
